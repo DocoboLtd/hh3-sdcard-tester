b9aaf1d7fed61065bc4de128161f980ec47c9cb4
shavinda-docobo <shavinda.rabichandran@docobo.co.uk>
2019-08-29 09:40:44 +0100

    Updated copyright statement for all files.

--- app/src/test/java/com/docobo/hh3sdcardtester/ExampleUnitTest.java
+++ app/src/test/java/com/docobo/hh3sdcardtester/ExampleUnitTest.java
@@ -1,11 +1,8 @@
 /*
- * *
- *  * Copyright (C) 2002 to Present, Docobo Ltd.
- *  * <br><br>
- *  * <b>Class ${NAME}</b>
- *  * <br><br>
- *  * TODO - Add class definition here.
+ *  Copyright (C) 2010 Docobo Ltd - All Rights Reserved
  *
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
  */
 
 package com.docobo.hh3sdcardtester;
